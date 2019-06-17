4b1068ad61e6557d2985149e07e598867424a950
popov-aa <popov.spb@gmail.com>
2019-06-17 23:09:04 +0300

    78

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,7 +3,7 @@ import './app.css'
 import Header from '../header'
 import ItemList from '../item-list'
 import RandomPlanet from '../random-planet'
-import ItemDetails from '../item-details'
+import ItemDetails, {Record} from '../item-details'
 import SwapiService from '../../services/swapi-service';
 import PeoplePage from '../people-page/people-page';
 import ErrorBoundry from '../error-boundry/error-boundry';
@@ -61,14 +61,21 @@ export default class App extends React.Component {
                 getData={this.swapiService.getPerson}
                 getImageUrl={this.swapiService.getPersonImage}
                 itemId={11}
-            />
+            >
+                <Record field="gender" label="Gender"/>
+                <Record field="eyeColor" label="Eye color"/>
+            </ItemDetails>
         );
         const starshipDetails = (
             <ItemDetails
                 getData={this.swapiService.getStarship}
                 getImageUrl={this.swapiService.getStarshipImage}
                 itemId={11}
-            />
+            >
+                <Record field="model" label="Model"/>
+                <Record field="length" label="Length"/>
+                <Record field="costInCredits" label="Cost"/>
+            </ItemDetails>
         );
 
         return (
--- src/components/item-details/item-details.js
+++ src/components/item-details/item-details.js
@@ -2,6 +2,19 @@ import React from 'react';
 import './item-details.css'
 import SwapiService from "../../services/swapi-service";
 
+const Record = ({item, field, label}) => {
+    return (
+        <li className="list-group-item">
+            <span className="term">{label}</span>
+            <span>{item[field]}</span>
+        </li>
+    );
+}
+
+export {
+    Record
+}
+
 export default class ItemDetails extends React.Component {
 
     swapiService = new SwapiService()
@@ -36,7 +49,10 @@ export default class ItemDetails extends React.Component {
                 <div className="item-details card">
                     <img className="item-image" src={image} alt="item image"/>
                     <div className="card-body">
-                        <b>{item.name}</b>
+                        <h4>{item.name}</h4>
+                        <ul className="list-group list-group-flush">
+                            {React.Children.map(this.props.children, (child) => {return React.cloneElement(child, {item})})}
+                        </ul>
                     </div>
                 </div>
             );
