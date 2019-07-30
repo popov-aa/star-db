8ff9a29df432ad1d3ff05c60f7a910740388db04
Попов Алексей <a.popov@ians.aero>
2019-07-30 17:48:09 +0300

    96

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './header.css'
+import {Link} from 'react-router-dom'
 
 const Header = ({onServiceChange}) => {
     return (
@@ -9,6 +10,11 @@ const Header = ({onServiceChange}) => {
                     StarDB
                 </a>
             </h3>
+            <ul className="d-flex">
+                <li><Link to="/people">People</Link></li>
+                <li><Link to="/planets">Planets</Link></li>
+                <li><Link to="/starships">Starships</Link></li>
+            </ul>
             <button
                 onClick={() => onServiceChange()}
                 className="btn btn-primary btn-sm">
