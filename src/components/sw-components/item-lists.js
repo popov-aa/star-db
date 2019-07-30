023dcc442b99755c3d5ab30a11c883d7fefc921a
Попов Алексей <a.popov@ians.aero>
2019-07-30 12:59:04 +0300

    89

--- src/components/sw-components/item-lists.js
+++ src/components/sw-components/item-lists.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ItemList from '../item-list';
-import {withData, withSwapiService} from '../hoc-helpers';
-
-const withChildFunction = (fn) => (Wrapped) => {
-    return (props) => {
-        return (
-            <Wrapped {... props}>
-                {fn}
-            </Wrapped>
-        )
-    }
-}
-
+import {withData, withSwapiService, compose, withChildFunction} from '../hoc-helpers';
 
 const renderName = ({name}) => <span>{name}</span>
 const renderModelAndName = ({model, name}) => <span>{name} ({model})</span>
@@ -19,28 +8,29 @@ const renderModelAndName = ({model, name}) => <span>{name} ({model})</span>
 const mapPersonsMethodsToProps = (swapiService) => {
     return { getData: swapiService.getAllPeople }
 };
-const PersonList = withSwapiService(mapPersonsMethodsToProps)(
-    withData(
-        withChildFunction(renderName)(ItemList)
-    )
-);
+const PersonList = compose(
+    withSwapiService(mapPersonsMethodsToProps),
+    withData,
+    withChildFunction(renderName)
+)(ItemList);
 
 const mapPlanetsMethodsToProps = (swapiService) => {
     return { getData: swapiService.getAllPlanets }
 };
-const PlanetList = withSwapiService(mapPlanetsMethodsToProps)(
-    withData(
-        withChildFunction(renderName)(ItemList)
-    )
-);
+const PlanetList = compose(
+    withSwapiService(mapPlanetsMethodsToProps),
+    withData,
+    withChildFunction(renderName)
+)(ItemList);
 
 const mapStarshipsMethodsToProps = (swapiService) => {
     return { getData: swapiService.getAllStarships }
 };
-const StarshipList = withSwapiService(mapStarshipsMethodsToProps)(
-    withData(withChildFunction(renderModelAndName)(ItemList)
-    )
-);
+const StarshipList = compose(
+    withSwapiService(mapStarshipsMethodsToProps),
+    withData,
+    withChildFunction(renderName)
+)(ItemList);
 
 export {
     PersonList,
